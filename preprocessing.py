989f5230914c817f3bf51a45b3b0a716ae013316
“Lazer2077” <709245585@qq.com>
2021-12-07 18:00:26 +0800

    v3.0

--- preprocessing.py
+++ preprocessing.py
@@ -11,6 +11,17 @@ def d_OpenDir():
 def gamma_adjust(im, gamma=1.0):
     """伽马矫正"""
     return (np.power(im.astype(np.float32)/255, 1/gamma)*255).astype(np.uint8)
+def Rotate(src,rotate_mat):
+    rows, cols = src.shape
+    img_zeros = np.zeros((rows,cols), np.uint8)
+    img_zeros.fill(255)
+    for x in range(rows):  
+        for y in range(cols):
+            if src[x,y]==0:
+                new_x,new_y=rotate_mat*np.mat([[x],[y]])
+                if 0<int(new_x)<rows and 0<int(new_y)<cols:
+                    img_zeros[int(new_x),int(new_y)]=0
+
 def rotation_normalization(src):
     total_x=0
     total_y=0
@@ -50,13 +61,21 @@ def rotation_normalization(src):
     v_sqbar=v_sq/N
     mu=mu_sum/N
     I = np.mat([[u_sqbar, mu], [mu, v_sqbar]])
-    print("MatrixI"+str(I))
+    #print("MatrixI"+str(I))
     eig_value,eig_vector=np.linalg.eig(I)
     rad=math.atan2(eig_vector[0,0],eig_vector[1,0])
-    print(rad/(np.pi)*180)
+    rad+=(math.pi/2)
+    print(rad)
+    R=np.mat([[math.cos(rad),-math.sin(rad)],[math.sin(rad),math.cos(rad)]])
+    print(R)
+    
     if flag:
+        if(rad<math.pi/18):
+            img_zeros=Rotate(img_zeros,R)
         return img_zeros
     else:
+        if(rad>math.pi/18):
+            src=Rotate(src,R)
         return src
     # for x in range(rows):   
     #     for y in range(cols):
@@ -78,8 +97,8 @@ for i in range(10):
     last="_{}.png".format(i+1)
     # ext='picture_ano_lzt_1/{}.jpg'.format(i+1)
     # exwrt='picture_ano_lzt_1/lzt_1/{}.png'.format(i+1)
-    mat_path= mat_path+human[1]+verbs[1]+last#mat_path+ext
-    wrt_path= wrt_path+human[1]+verbs[1]+last#mat_path+exwrt
+    mat_path= mat_path+human[0]+verbs[4]+last#mat_path+ext
+    wrt_path= wrt_path+human[0]+verbs[4]+last#mat_path+exwrt
     print(mat_path)
     mat=cv.imread(mat_path)
     rows=mat.shape[0]
